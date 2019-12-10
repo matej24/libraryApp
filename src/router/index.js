ac5514ee90355ce78a7f88ba6929848f48cc3476
msekulicfinsec <matej.sekulic@finance-security.eu>
2019-12-10 16:00:29 +0100

    router first step

--- src/router/index.js
+++ src/router/index.js
@@ -10,16 +10,23 @@ const routes = [
     name: 'home',
     component: Home
   },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: function () {
+  //     return import(/* webpackChunkName: "about" */ '../views/About.vue')
+  //   }
+  // },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: '/books/:id',
+    name: 'book-view',
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+      return import(/* webpackChunkName: "about" */ '../views/BookView.vue')
     }
-  }
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/BookView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<script>
+
+
+export default {
+  name: 'home',
+
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    console.log(this.$route.params.id)
+  },
+  methods: {
+
+  }
+
+}
+</script>
